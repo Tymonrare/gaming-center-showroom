fd6d623fd52ca9c62c42e5c194c8ea9446836977
Tymonr <Tymonr@yandex.ru>
2018-07-08 22:32:16 +0300

    xd ready

---
+++ XD/default/src/script.js
@@ -0,0 +1,77 @@
+(function(){
+	var _defaultView = "not_connected"
+	var statusViewName = null;
+	
+	var config;
+	window.machineStatus = {};
+
+	var bind = {
+		machine_status:"unconnected"
+	}
+	
+	window.main = function(){	
+		utils.bind.val.dynamic = bind;
+		utils.bind.defineDynamic();
+		
+		$.getJSON( "res/config.json", function( data ) {
+			config = data;
+			startSockets();
+			
+			loadView(_defaultView);
+		});
+	};
+	
+	function putStatusInfoInBinds(){
+		bind.machine_status = machineStatus.Status;
+		bind.playing_game = machineStatus.Game;
+		bind.configured_playtime = machineStatus.ConfiguredPlayTime;
+	}
+	
+	function startSockets(){
+		var socket = new WebSocket("ws://"+config.masterServerIp+"/status");
+		
+		socket.onmessage = function(event){
+			var msg = JSON.parse(event.data);
+			
+			if(msg.Status){
+				machineStatus = msg;
+				putStatusInfoInBinds();
+				loadStatusView(machineStatus.Status);
+			}
+			else
+				bind.machine_status = "unknown";
+		}
+		
+		//spam reconnection
+		socket.onclose = function(){
+			setTimeout(startSockets, 1000);
+			bind.machine_status = "unconnected";
+			loadView(_defaultView);
+		}
+	}
+	
+	function loadStatusView(view){
+		loadView("status-"+view);
+	}
+	
+	function loadView(view){
+		if(statusViewName == view) return;
+		
+		$("#root").load("views/"+view+".html", function(responseText, textStatus, req){
+			if (textStatus == "error") return;
+
+			statusViewName = view;
+			applyNewView();
+		});
+		window.scrollTo(0,0);
+	}
+	
+	window.dbg_loadView = loadView;
+
+	function applyNewView(){
+		$.getJSON("res/translations/" + config.language + ".json", function(data){
+			utils.bind.val.static = data;
+			utils.bind.parseBindableChilds(document.body);
+		})
+	}
+})();
