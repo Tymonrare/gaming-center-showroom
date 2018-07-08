21dd03cb1776c5e6502270b587b3d6aec61f8636
Tymonr <Tymonr@yandex.ru>
2018-07-08 13:08:36 +0300

    improves

--- template/server.py
+++ template/server.py
@@ -1,11 +1,33 @@
 import http.server
 import socketserver
 import sys
+import os
+import json
 
-PORT = int(sys.argv[1])
+#====
+#update videos list
+videos_dir = "res/videos/"
+json_video_list_file = videos_dir + "list.json"
+
+if os.path.isfile(json_video_list_file):
+	os.remove(json_video_list_file)
+	
+json_video_list = os.listdir(videos_dir)
+
+#save to json
+with open(json_video_list_file, 'w') as json_video_list_file:
+    json.dump(json_video_list, json_video_list_file, sort_keys=True, indent=2, ensure_ascii=False, separators=(',', ': '))
+
+#====
+#start server
+PORT = 8000
+
+if len(sys.argv) > 1:
+	PORT = int(sys.argv[1])
 
 Handler = http.server.SimpleHTTPRequestHandler
 
+#begin listening
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
