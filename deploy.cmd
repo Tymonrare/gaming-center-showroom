9170c8aa28ca998f0761b2499971f3467c4c3513
Tymonr <Tymonr@yandex.ru>
2018-07-14 23:37:11 +0300

    fixes

---
+++ deploy.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+set /P sh_type="Enter showroom type (xd-default for example): " %=%
+
+del "%sh_type%.7z"
+
+"C:\Program Files\7-Zip\7z.exe" a -t7z %sh_type%.7z -r .\%sh_type%
+
+python ../GamingCenter/maintenance/deploy-new-bin.py ./%sh_type%.7z https://d3i8w87ms3cidi.cloudfront.net bin/showrooms/%sh_type%-"
+
